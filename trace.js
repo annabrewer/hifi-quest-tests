ca2f2534e24773034f702a126c5afea9633baf8b
Anna <anna@highfidelity.io>
2019-06-17 16:42:19 -0700

    made start time later so hq could finish loading

--- trace.js
+++ trace.js
@@ -1,7 +1,7 @@
 //Window.location = "hifi://localhost/-1.5,-0.1,-0.5/0,-0.6,0,-0.8";
 Window.location = "hifi://engine-dev/21.9769,-9.70012,-21.5019/0,0.891658,0,0.45271";
 
-var startTime = 5;
+var startTime = 30;
 var endTime = startTime + 30;
 var outputFile = "/sdcard/Documents/trace-all.json.gz";
 
